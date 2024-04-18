2f40da6aadbb2479c139596548e56e0532d343c8
mughilan16 <mughilant@proton.me>
2024-04-18 13:38:43 +0530

    initial commit

---
+++ nvim/lua/mughilan/plugins/nvim-tree.lua
@@ -0,0 +1,22 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  dependencies = "nvim-tree/nvim-web-devicons",
+  config = function()
+    vim.keymap.set('n', '<C-n>', ":NvimTreeToggle<CR>", {})
+    require("nvim-tree").setup({
+      sort = {
+        sorter = "case_sensitive",
+      },
+      view = {
+        width = 35,
+      },
+      renderer = {
+        group_empty = true,
+      },
+      filters = {
+        dotfiles = true,
+      },
+    })
+    vim.cmd[[":NvimTreeOpen<CR>"]]
+  end
+}
